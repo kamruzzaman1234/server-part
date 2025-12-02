ce5c556420c0f8a28b7fd97cefad02750937c1ed
MD Kamruzzaman Labib <MD Kamruzzaman>
2025-12-02 10:42:46 +0600

    added authController register code

--- models/userModel.js
+++ models/userModel.js
@@ -11,3 +11,7 @@ const userSchema = new mongoose.Schema({
     resetOtp:{type: String, default: ''},
     resetOtpExpireAt: {type: Number, default: 0} 
 })
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
